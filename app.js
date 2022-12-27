d5118d6d56c2cf662af0c93578bca380545a9d9e
guille garcia <ggarciacardiel@gmail.com>
2022-12-27 21:20:24 +0100

    added presents.hbs + registered partials in app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 require('./db');
-
+const hbs = require('hbs');
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
 const presentsRouter = require('./routes/presents')
@@ -14,6 +14,7 @@ const app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
+hbs.registerPartials(__dirname + '/views/partials'); 
 
 app.use(logger('dev'));
 app.use(express.json());
